b38e92eb36ef6f470452b70ef4bff2fe6afe90e6
Le Minh <le.minh011100@gmail.com>
2022-10-27 12:45:21 +0700

    remove serverless

--- src/services/file.js
+++ src/services/file.js
@@ -1,14 +1,11 @@
 import instance from './axios';
 
 export const uploadSingle = async (data) =>
-  await fetch('/api/upload', {
+  await instance({
+    url: '/image/upload',
     method: 'POST',
-    // headers: {
-    //   "Content-Type": "multipart/form-data"
-    // },
-    body: data,
-  })
-    .then((response) => response.json());
+    data,
+  });
 
 export const remove = async (data) =>
   await instance.post('/image/remove', data);
