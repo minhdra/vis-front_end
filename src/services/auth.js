0dd5a17e10bb0e384f479745445c50c4f955c34f
Le Minh <le.minh011100@gmail.com>
2022-10-29 14:50:19 +0700

    fixed auth bug

--- src/services/auth.js
+++ src/services/auth.js
@@ -1,27 +1,76 @@
 import instance from './axios';
 
-export const search = async (data) => (await instance.post('/auth/search', {
-  ...data
-})).data;
+const token = window.sessionStorage.getItem('USER_TOKEN');
 
-export const getById = async (id) => (await instance.get('/auth/' + id)).data;
+export const search = async (data) =>
+  (
+    await instance.post(
+      '/auth/search',
+      {
+        ...data,
+      },
+      {
+        headers: {
+          'auth-token': token,
+        },
+      }
+    )
+  ).data;
 
-export const register = async (data) => (await instance.post('/auth/register', {
-  ...data
-}));
+export const getById = async (id) =>
+  (
+    await instance.get('/auth/' + id, {
+      headers: {
+        'auth-token': token,
+      },
+    })
+  ).data;
 
-export const login = async (data) => (await instance.post('/auth/login', {
-  ...data
-}));
+export const register = async (data) =>
+  await instance.post('/auth/register', {
+    ...data,
+  });
 
-export const changePassword = async (data) => (await instance.post('/auth/change-password', {
-  ...data
-}));
+export const login = async (data) =>
+  await instance.post('/auth/login', {
+    ...data,
+  });
 
-export const remove = async (data) => (await instance.post('/auth/delete', {
-  ...data
-}));
+export const changePassword = async (data) =>
+  await instance.post(
+    '/auth/change-password',
+    {
+      ...data,
+    },
+    {
+      headers: {
+        'auth-token': token,
+      },
+    }
+  );
 
-export const update = async (data) => (await instance.post('/auth/update', {
-  ...data
-}));
+export const remove = async (data) =>
+  await instance.post(
+    '/auth/delete',
+    {
+      ...data,
+    },
+    {
+      headers: {
+        'auth-token': token,
+      },
+    }
+  );
+
+export const update = async (data) =>
+  await instance.post(
+    '/auth/update',
+    {
+      ...data,
+    },
+    {
+      headers: {
+        'auth-token': token,
+      },
+    }
+  );
